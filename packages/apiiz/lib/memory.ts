a17d89464b2ae8e314ae629d980594e35cbeb898
linq2js <linqtojs@gmail.com>
2022-05-07 22:20:42 +0700

    docs

--- packages/apiiz/lib/memory.ts
+++ packages/apiiz/lib/memory.ts
@@ -1,6 +1,6 @@
-import { Context, Resolver } from "./types";
+import { ResolverContext, Resolver } from "./types";
 
 export const memory =
-  <P, R>(fn: (payload: P, context: Context) => R): Resolver<P, R> =>
+  <P, R>(fn: (payload: P, context: ResolverContext) => R): Resolver<P, R> =>
   (context) =>
     ((payload: P) => fn(payload, context)) as any;
