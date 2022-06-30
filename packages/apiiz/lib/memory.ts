9c7975691eed493e499ee1bd368276b872266860
linq2js <linqtojs@gmail.com>
2022-07-01 06:11:06 +0700

    fix: typing docs

--- packages/apiiz/lib/memory.ts
+++ packages/apiiz/lib/memory.ts
@@ -1,11 +1,9 @@
 import { Context, Resolver } from "./types";
 
 export interface Memory {
-  <R>(fn: () => R): Resolver<void, R>;
-  <P, R>(fn: (payload: P, context: Context) => R): Resolver<P, R>;
+  <R>(fn: () => R | Promise<R>): Resolver<void, R>;
+  <P, R>(fn: (payload: P, context: Context) => R | Promise<R>): Resolver<P, R>;
 }
 
-
-export const memory: Memory = (fn: Function) =>
-  (context: any) =>
-    ((payload: any) => fn(payload, context)) as any;
+export const memory: Memory = (fn: Function) => (context: any) =>
+  ((payload: any) => fn(payload, context)) as any;
